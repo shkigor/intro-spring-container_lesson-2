f795b18aaaa1af6136c0aa8105e9d1c470c36861
Igor Shklyar <igor.shklyar@gmail.com>
2019-03-13 12:05:30 +0200

    Lesson_02. Жизненный цикл бина. Для управления контейнером жизненным циклом бина, вы можете реализовать метод afterPropertiesSet() интерфейса InitializingBean и метод destroy() интерфейса DisposableBean. Однако их не рекомендуется использовать, поскольку они дублируют код Spring.

--- src/main/java/lessons/services/GreetingServiceImpl.java
+++ src/main/java/lessons/services/GreetingServiceImpl.java
@@ -1,8 +1,38 @@
 package lessons.services;
 
-public class GreetingServiceImpl implements GreetingService {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class GreetingServiceImpl implements GreetingService, InitializingBean, DisposableBean {
+
+    private static final Logger logger = LogManager.getLogger(GreetingServiceImpl.class);
+
     @Override
     public String sayGreeting() {
         return "Greeting, user!";
+    }
+
+    /**
+     * Метод в интерфейсе InitializingBean.
+     * Позволяет выполнять какие-либо действия после инициализации всех свойств бина контейнером
+     * afterPropertiesSet() and destroy() не рекомендуется использовать, поскольку они дублируют код Spring.
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("******* INVOKE afterPropertiesSet() METHOD");
+    }
+
+    /**
+     * Метод в интерфейсе DisposableBean.
+     * Позволяет выполнять какие-либо действия при уничтожении бина контейнером
+     * afterPropertiesSet() and destroy() не рекомендуется использовать, поскольку они дублируют код Spring.
+     * @throws Exception
+     */
+    @Override
+    public void destroy() throws Exception {
+        logger.info("invoke destroy method");
     }
 }
