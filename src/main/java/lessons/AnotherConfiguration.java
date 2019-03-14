dd1feeae3d4135fccacac7cd570a9ea4a37f6d4f
Igor Shklyar <igor.shklyar@gmail.com>
2019-03-14 13:53:42 +0200

    Lesson_02. Использование @Configuration аннотации В большинстве случаев, имеются такие случаи, когда бин в одной конфигурации имеет зависимость от бина в другой конфигурации. Поскольку конфигурация является источником определения бинов, то разрешить такую зависимость не является проблемой, достаточно объявить поле класса конфигурации с аннотацией @Autowired
    Классы с аннотацией @Configuration не стремятся на 100% заменить конфигурации на XML, при этом, если вам удобно или имеется какая-то необходимость в использовании XML конфигурации, то к вашей Java-конфигурации необходимо добавить аннотацию @ImportResource, в параметрах которой необходимо указать нужное вам количество XML-конфигураций.

--- src/main/java/lessons/AnotherConfiguration.java
+++ src/main/java/lessons/AnotherConfiguration.java
@@ -1,13 +1,20 @@
 package lessons;
 
 import lessons.services.BeanWithDependency;
+import lessons.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AnotherConfiguration {
+
+    @Autowired
+    GreetingService greetingService;
+
     @Bean
     BeanWithDependency beanWithDependency() {
+        //что-нибудь делаем с greetingService...
         return new BeanWithDependency();
     }
 }
